da07eb96db3d8aa7540692b69843f2e582af8665
Mark Heily <mark@heily.com>
2016-10-23 14:33:01 -0400

    Initial Ruby cleanup effort

---
+++ lib/ruby/room.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2016 Mark Heily <mark@heily.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+require "json"
+require "pp"
+require 'logger'
+require 'tempfile'
+
+class Room
+  def setup_logger
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::DEBUG
+  end
+  
+  def setup_tmpdir
+    @tmpdir = Dir.mktmpdir($PROGRAM_NAME)
+    at_exit { system "rm -rf #{@tmpdir}" if File.exist?(@tmpdir) }
+  end
+    
+  def logger
+    @logger
+  end
+  
+  def parse_options_json(path)
+    data = `uclcmd get --file #{path} -c ''`
+    json = JSON.parse data
+    logger.debug "json=#{json.pretty_inspect}"
+    json
+  end
+  
+  def system(command)
+    logger.debug 'running: ' + command
+    Kernel.system(command)
+  end
+end
